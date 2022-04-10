858b56da1aedf3f0565aa353425f20c4f50483ac
肇鑫（Zhao Xin） <owenzx@gmail.com>
2022-04-10 14:42:41 +0800

    * removed restTimeDidChanged.

--- Sources/SpeedTestServiceNotification/SpeedTestServiceNotification.swift
+++ Sources/SpeedTestServiceNotification/SpeedTestServiceNotification.swift
@@ -1,8 +1,6 @@
 import Foundation
 
 public struct SpeedTestServiceNotification {
-    public static let restTimeDidChanged = Notification.Name("restTimeDidChanged")
-    
     public static let start = Notification.Name("start")
     public static let stop = Notification.Name("stop")
 
