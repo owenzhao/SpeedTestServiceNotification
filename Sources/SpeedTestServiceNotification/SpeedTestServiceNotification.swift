96783fa0f0910ac22eac26ff6a60113764c5b465
肇鑫（Zhao Xin） <owenzx@gmail.com>
2022-04-13 14:13:30 +0800

    * removed unused code.

--- Sources/SpeedTestServiceNotification/SpeedTestServiceNotification.swift
+++ Sources/SpeedTestServiceNotification/SpeedTestServiceNotification.swift
@@ -3,7 +3,4 @@ import Foundation
 public struct SpeedTestServiceNotification {
     public static let start = Notification.Name("start")
     public static let stop = Notification.Name("stop")
-
-    public init() {
-    }
 }
